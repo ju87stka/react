627499f14947215132b4bcd877fd8f78e67c488f
ju87stka <your_email@whatever.com>
2020-03-22 22:17:41 +0300

    Commit 2 UserPage

---
+++ src/redux/users-reducer.js
@@ -0,0 +1,56 @@
+
+const  ADD_POST="ADD-POST";
+const  UPDATE_NEW_POST_TEXT='UPDATE-NEW-POST-TEXT'
+
+let initialState= {             posts: [
+        {id: 1, message: "First post", likesCount: 12},
+        {id: 2, message: "Second post", likesCount: 6},
+    ],
+    newPostText: "igor"
+
+};
+
+ const profileReducer=(state=initialState, action)=>{
+    switch (action.type) {
+        case ADD_POST: {
+            let newPost = {
+                id: 5,
+                message: state.newPostText,
+                likesCount: 0
+            };
+            let stateCopy = {...state};
+            stateCopy.posts = [...state.posts];
+            stateCopy.posts.push(newPost);
+            stateCopy.newPostText = "";
+
+            return stateCopy;
+        }
+
+
+
+        case UPDATE_NEW_POST_TEXT : {
+            let stateCopy = {...state};
+
+            stateCopy.newPostText = action.newText;
+            return stateCopy;
+        }
+        default:
+            return state;
+    }
+
+
+
+
+
+}
+export  const addPostActionCreator=()=>   {
+    return{
+        type: ADD_POST
+    }
+}
+export const updateNewPostTextActionCreator=(text)=>   {
+    return{
+        type:UPDATE_NEW_POST_TEXT,
+        newText : text    }
+}
+export default profileReducer;
