e3a9e5bbe2b6899d0a78eda2e1c863dbee98cb14
ju87stka <your_email@whatever.com>
2020-03-24 12:10:08 +0300

    Commit 3

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,9 +1,17 @@
 import React from "react";
 import  styles from "./users.module.css";
-
+import * as axios from "axios";
+import  userPhoto from "../../assets/images/user.png"
 let  Users =(props)=>{
     if (props.users.length === 0) {
-    props.setUsers(
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+            props.setUsers(response.data.items);
+
+
+        });
+
+   /* props.setUsers(
         [
         {
             id: 1,
@@ -37,14 +45,15 @@ let  Users =(props)=>{
 
 
     ]
-    )
+    )*/
     }
     return <div>
         {
             props.users.map(u=> <div key={u.id}>
             <span>
                 <div>
-                    <img src={u.photoUrl} className={styles.userPhoto}/>
+
+                    <img src={u.photos.small !=null ? u.photos.small : userPhoto} className={styles.userPhoto}/>
 
                 </div>
                 <div>
@@ -57,13 +66,13 @@ let  Users =(props)=>{
             </span>
                 <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
                         
-                        <div>{u.location.city}</div>
-                        <div>{u.location.country}</div>
+                        <div>{"u.location.city"}</div>
+                        <div>{"u.location.country"}</div>
                     </span>
 
 
