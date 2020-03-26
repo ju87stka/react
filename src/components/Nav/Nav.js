212f14bf0fa70c46ea6e2526fd5d119b0645e635
ju87stka <your_email@whatever.com>
2020-03-26 14:29:07 +0300

    Pagination Users

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -31,7 +31,8 @@ const Nav = (props) => {
                             <NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink></div>
                         <div className={classes.item}>
                             <NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink></div>
-
+                        <div className={classes.item}>
+                            <NavLink to="/users" activeClassName={classes.active}>Users</NavLink></div>
                         <div className={classes.item}>
                             <NavLink to="/news" activeClassName={classes.active}>News</NavLink></div>
                         <div className={classes.item}>
