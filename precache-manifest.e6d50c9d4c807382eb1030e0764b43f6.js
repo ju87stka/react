7a1cfb76edf854aea4c7539631c739568b727e0a
ju87stka <your_email@whatever.com>
2020-04-26 20:09:36 +0300

    Updates

---
+++ precache-manifest.e6d50c9d4c807382eb1030e0764b43f6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4345d3306e9ea8bb48cfc37c41d93ec",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "0b767d8fb40063f7717a",
+    "url": "/react/static/css/main.12d66593.chunk.css"
+  },
+  {
+    "revision": "8c92533d95348cb236b4",
+    "url": "/react/static/js/2.75c3426f.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/react/static/js/2.75c3426f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b767d8fb40063f7717a",
+    "url": "/react/static/js/main.cfab806b.chunk.js"
+  },
+  {
+    "revision": "4b22802a02174c2d83af",
+    "url": "/react/static/js/runtime-main.ff8e6795.js"
+  },
+  {
+    "revision": "9064981b80e59f1b663c17fab41563c9",
+    "url": "/react/static/media/loading.9064981b.svg"
+  },
+  {
+    "revision": "e67e358c72e30a5ec593534154b91ea6",
+    "url": "/react/static/media/user1.e67e358c.png"
+  }
+]);
